1b79add672239b7b6999a556272047dee8e75714
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-01-04 01:36:57 +0900

    コメント追加

--- src/const_val.py
+++ src/const_val.py
@@ -4,7 +4,7 @@ from enum import Enum, auto
 
 # 移動方向
 class Direction(Enum):
-    Up = auto()
-    Down = auto()
-    Left = auto()
-    Right = auto()
+    Up = auto() # 上
+    Down = auto() # 下
+    Left = auto() # 左
+    Right = auto() # 右
