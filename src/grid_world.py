1e03b4e74ecd4f9701accae69fe40577c22bfd90
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-01-05 00:06:44 +0900

    移動できる方向を事前に取得できるようにした それに伴い、指定方向に移動できるかどうか判定する処理を関数化 メンバ変数やそれに関連する変数の名称を改善

--- src/grid_world.py
+++ src/grid_world.py
@@ -1,5 +1,7 @@
 # 強化学習勉強用サンプルプログラム Grid World環境クラス
 
+from copy import copy
+
 from const_val import *
 
 # Grid World環境クラス
@@ -7,42 +9,61 @@ class GridWorld:
     # コンストラクタ
     def __init__(self):
         # 盤面の作成
-        self.wall, self.cell_idx, self.goal_idx = self._create_grid()
+        self.wall, self.pos, self.goal_pos = self._create_grid()
 
     # 移動する
     def move(self, direction):
-        # すでにゴールにいる場合は移動しない
-        if self.cell_idx in self.goal_idx:
-            print(f'すでにゴール座標 {self.cell_idx} にいます。')
-            return self.cell_idx
-
-        idx_diff = {
+        pos_diff = {
             Direction.Up: [0, -1],
             Direction.Down: [0, 1],
             Direction.Left: [-1, 0],
             Direction.Right: [1, 0],
         }[direction]
 
-        next_idx = [i + d for i, d in zip(self.cell_idx, idx_diff)]
+        next_pos = [i + d for i, d in zip(self.pos, pos_diff)]
+
+        if self._can_move(direction):
+            self.pos = copy(next_pos)
+
+    # 移動できる方向の検索
+    def get_available_direction(self):
+        available_direction = []
+        for name in Direction._member_names_:
+            if self._can_move(Direction[name]):
+                available_direction.append(Direction[name])
+
+        return available_direction
+
+    # 指定した方向に移動できるかどうか判定する
+    def _can_move(self, direction):
+        pos_diff = {
+            Direction.Up: [0, -1],
+            Direction.Down: [0, 1],
+            Direction.Left: [-1, 0],
+            Direction.Right: [1, 0],
+        }[direction]
+
+        next_pos = [i + d for i, d in zip(self.pos, pos_diff)]
+
+        # すでにゴールにいる場合は移動しない
+        if self.pos in self.goal_pos:
+            return False
 
         # 範囲内の判定
-        x_is_in_grid = (0 <= next_idx[0] < GRID_WIDTH)
-        y_is_in_grid = (0 <= next_idx[1] < GRID_HEIGHT)
+        x_is_in_grid = (0 <= next_pos[0] < GRID_WIDTH)
+        y_is_in_grid = (0 <= next_pos[1] < GRID_HEIGHT)
         if (not x_is_in_grid) or (not y_is_in_grid):
-            print(f'グリッド外の座標 {next_idx} に移動しようとしています。')
-            return self.cell_idx
+            return False
 
         # 壁の判定
-        hit_vertical_wall = ((direction == Direction.Left) and (self.wall[2 * next_idx[1]][next_idx[0]])) or \
-                            ((direction == Direction.Right) and (self.wall[2 * next_idx[1]][next_idx[0] - 1]))
-        hit_horizontal_wall = ((direction == Direction.Up) and (self.wall[2 * next_idx[1] + 1][next_idx[0]])) or \
-                              ((direction == Direction.Down) and (self.wall[2 * next_idx[1] - 1][next_idx[0]]))
+        hit_vertical_wall = ((direction == Direction.Left) and (self.wall[2 * next_pos[1]][next_pos[0]])) or \
+                            ((direction == Direction.Right) and (self.wall[2 * next_pos[1]][next_pos[0] - 1]))
+        hit_horizontal_wall = ((direction == Direction.Up) and (self.wall[2 * next_pos[1] + 1][next_pos[0]])) or \
+                              ((direction == Direction.Down) and (self.wall[2 * next_pos[1] - 1][next_pos[0]]))
         if hit_vertical_wall or hit_horizontal_wall:
-            print(f'座標 {self.cell_idx} から {next_idx} に移動しようしたとき、壁に衝突しました。')
-            return self.cell_idx
+            return False
 
-        print(f'座標 {self.cell_idx} から {next_idx} に移動しました。')
-        self.cell_idx = next_idx
+        return True
 
     # 盤面(グリッド)を作成
     def _create_grid(self):
@@ -66,10 +87,10 @@ class GridWorld:
         ]
         # 初期位置の定義
         # 盤面の幅がW, 高さがHの時、左上を(0, 0)、右下を(W - 1, H - 1)とする。
-        initial_idx = [0, 0]
+        initial_pos = [0, 0]
         # ゴール位置の定義
         # 複数存在することを想定し、リストにする。
-        goal_idx = [[GRID_WIDTH - 1, GRID_HEIGHT - 1]]
+        goal_pos = [[GRID_WIDTH - 1, GRID_HEIGHT - 1]]
 
-        return wall, initial_idx, goal_idx
+        return wall, initial_pos, goal_pos
 
