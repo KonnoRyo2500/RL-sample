da520c6ca06901e411e447e23c048791a3a5fd3d
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-01-05 21:37:57 +0900

    不要なメンバ変数、関数の削除 定数にできるものは定数にした

--- src/const_val.py
+++ src/const_val.py
@@ -2,9 +2,38 @@
 
 from enum import Enum, auto
 
+### Grid World環境定義 ###
+
 # 盤面のサイズ
 GRID_WIDTH = 5
 GRID_HEIGHT = 5
+
+# 初期位置
+# 盤面の幅がW, 高さがHの時、左上を(0, 0)、右下を(W - 1, H - 1)とする。
+INITIAL_POS = [0, 0]
+
+# ゴール位置
+# 複数存在することを想定し、リストにする。
+GOAL_POS = [[GRID_WIDTH - 1, GRID_HEIGHT - 1]]
+
+# 壁情報
+# 定義方法はドキュメントを参照。
+# 事前に手書きやペイントソフト、Excelなどで盤面を描いておくと良い。
+WALL = [
+    (False, False, True, False), # 1列目の垂直方向の壁情報
+    (True, False, False, True, False), # 1列目の水平方向の壁情報
+
+    (True, True, False, False),
+    (False, False, True, False, True),
+
+    (False, False, True, False),
+    (False, True, True, True, False),
+
+    (False, False, False, True),
+    (False, False, True, False, True),
+
+    (True, False, True, False),
+]
 
 # 移動方向
 class Direction(Enum):
--- src/grid_world.py
+++ src/grid_world.py
@@ -8,9 +8,7 @@ from const_val import *
 class GridWorld:
     # コンストラクタ
     def __init__(self):
-        # 盤面の作成
-        self.wall, self.init_pos, self.goal_pos = self._create_grid()
-        self.pos = copy(self.init_pos)
+        self.pos = copy(INITIAL_POS)
 
     # 移動する
     def move(self, direction):
@@ -21,7 +19,7 @@ class GridWorld:
 
     # 盤面のリセット
     def reset(self):
-        self.pos = copy(self.init_pos)
+        self.pos = copy(INITIAL_POS)
 
     # 移動できる方向の検索
     def get_available_direction(self):
@@ -50,7 +48,7 @@ class GridWorld:
         next_pos = self._get_next_pos(direction)
 
         # すでにゴールにいる場合は移動しない
-        if self.pos in self.goal_pos:
+        if self.pos in GOAL_POS:
             return False
 
         # 範囲内の判定
@@ -60,41 +58,12 @@ class GridWorld:
             return False
 
         # 壁の判定
-        hit_vertical_wall = ((direction == Direction.Left) and (self.wall[2 * next_pos[1]][next_pos[0]])) or \
-                            ((direction == Direction.Right) and (self.wall[2 * next_pos[1]][next_pos[0] - 1]))
-        hit_horizontal_wall = ((direction == Direction.Up) and (self.wall[2 * next_pos[1] + 1][next_pos[0]])) or \
-                              ((direction == Direction.Down) and (self.wall[2 * next_pos[1] - 1][next_pos[0]]))
+        hit_vertical_wall = ((direction == Direction.Left) and (WALL[2 * next_pos[1]][next_pos[0]])) or \
+                            ((direction == Direction.Right) and (WALL[2 * next_pos[1]][next_pos[0] - 1]))
+        hit_horizontal_wall = ((direction == Direction.Up) and (WALL[2 * next_pos[1] + 1][next_pos[0]])) or \
+                              ((direction == Direction.Down) and (WALL[2 * next_pos[1] - 1][next_pos[0]]))
         if hit_vertical_wall or hit_horizontal_wall:
             return False
 
         return True
-
-    # 盤面(グリッド)を作成
-    def _create_grid(self):
-        # 盤面の各マスの周りにある壁情報を定義する。
-        # 定義方法はドキュメントを参照。
-        # 事前に手書きやペイントソフト、Excelなどで盤面を描いておくと良い。
-        wall = [
-            (False, False, True, False), # 1列目の垂直方向の壁情報
-            (True, False, False, True, False), # 1列目の水平方向の壁情報
-
-            (True, True, False, False),
-            (False, False, True, False, True),
-
-            (False, False, True, False),
-            (False, True, True, True, False),
-
-            (False, False, False, True),
-            (False, False, True, False, True),
-
-            (True, False, True, False),
-        ]
-        # 初期位置の定義
-        # 盤面の幅がW, 高さがHの時、左上を(0, 0)、右下を(W - 1, H - 1)とする。
-        initial_pos = [0, 0]
-        # ゴール位置の定義
-        # 複数存在することを想定し、リストにする。
-        goal_pos = [[GRID_WIDTH - 1, GRID_HEIGHT - 1]]
-
-        return wall, initial_pos, goal_pos
 
