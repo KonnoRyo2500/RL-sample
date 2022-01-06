4e7385a0a79d3243304baf3fdb56aa81bd9a60fe
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-01-06 19:29:40 +0900

    エージェントベースクラスの各種関数を実装 とりあえず必要そうな関数を定義。今後関数が追加削除される可能性あり

--- src/agent/agent_base.py
+++ src/agent/agent_base.py
@@ -1,6 +1,31 @@
 # 強化学習勉強用サンプルプログラム エージェントベースクラス
 
+from abc import ABCMeta, abstractmethod
+
 # エージェントベースクラス
 # このクラスのインスタンスは作成できない(抽象クラス)
-class AgentBase:
-    pass
+# 具体的な強化学習アルゴリズムはサブクラスで実装すること。
+class AgentBase(metaclass=ABCMeta):
+    # コンストラクタ
+    def __init__(self, env):
+        self.env = env
+
+    # 学習した価値関数を基にエピソードをプレイ
+    @abstractmethod
+    def play(self):
+        pass
+
+    # 学習の実行
+    @abstractmethod
+    def train(self):
+        pass
+
+    # エピソードの実行
+    @abstractmethod
+    def episode(self):
+        pass
+
+    # 1ステップ実行
+    @abstractmethod
+    def step(self):
+        pass
