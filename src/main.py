1b21c67ee2d4460d9e5ee7114b580bfdfb33d9fa
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-01-09 20:03:51 +0900

    盤面定義用Excelシートのパスをコマンドラインで指定できるようにした

--- src/main.py
+++ src/main.py
@@ -1,12 +1,27 @@
 # 強化学習勉強用サンプルプログラム メイン処理
 
+import argparse
+
 from environment.grid_world import GridWorld
 from agent.q_agent import QAgent
 from agent.sarsa_agent import SarsaAgent
 
+# コマンドライン引数の解析
+def parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument(
+        '--grid_xlsx',
+        type=str,
+        default='..\\Grid.xlsx',
+        help='盤面情報を定義するExcelシートへのパス。')
+    args = parser.parse_args()
+    return args
+
 # メイン処理
 def main():
-    gw = GridWorld(wall_xlsx='..\\Grid.xlsx')
+    args = parse_args()
+
+    gw = GridWorld(wall_xlsx=args.grid_xlsx)
     q = QAgent(gw)
     q.train()
     q.play()
