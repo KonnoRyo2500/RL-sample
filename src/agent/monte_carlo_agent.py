78dbf3207dbd093defa9f5ab3379c40d12dbebd9
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-03-01 23:49:07 +0900

    モンテカルロ法のアルゴリズムを、方策オン型の初回訪問モンテカルロ法に変更した モンテカルロ法エージェントが正しく動作するようになった

--- src/agent/monte_carlo_agent.py
+++ src/agent/monte_carlo_agent.py
@@ -2,6 +2,7 @@
 
 import random
 from pprint import pprint
+from itertools import product
 
 from agent.agent_base import AgentBase
 
@@ -10,7 +11,7 @@ class MonteCarloAgent(AgentBase):
     # コンストラクタ
     def __init__(self, env, config):
         super().__init__(env, config)
-        self.v_func = self._make_initial_v_function()
+        self.q_func = self._make_initial_q_function()
 
     # 学習した価値関数を基にエピソードをプレイ
     def play(self):
@@ -40,65 +41,56 @@ class MonteCarloAgent(AgentBase):
 
     # 学習の実行
     def train(self):
-        # アルゴリズム詳細
-        # 1) 各状態sについて、状態価値関数V(s)を0で初期化
-        # 2) 各状態sを開始状態としてエピソードを実施し、収益
-        #       R = r0 + γr1 + γ^(2)r2 + ... + γ^(T-1)rT
-        #    を得る。
-        # 3) 2)を一定回数繰り返し、sにおける平均収益avg(R)を計算する。
-        # ※ただし、終端状態での状態価値関数の値は、その終端状態での報酬値とする。
-        # 4) V(s) = avg(R)として完了。
-        # Note: モンテカルロ法には複数の亜種が存在するが、
-        #       ここでは一番シンプルな方法を選択した。
+        # モンテカルロ法には複数の亜種が存在するが、
+        # ここでは方策オン型の初回訪問モンテカルロ法を採用する。
 
-        for s in self.env.get_state_space():
-            # 一定回数プレイアウトを繰り返し、平均収益を求める
-            profit_sum = 0
-            for i in range(self.config['num_playout']):
-                profit = self._playout(s)
-                if profit is None:
-                    profit_sum = None
-                    break
+        s_space = self.env.get_state_space()
+        a_space = self.env.get_action_space()
+
+        # Returnsの初期化
+        returns = {}
+        for state, action in product(s_space, a_space):
+            returns[(state, action)] = []
+
+        # プレイアウト実施 + 行動価値関数の更新
+        for _ in range(self.config['num_playout']):
+            # プレイアウトの実施
+            exp_history = self._playout()
+
+            # Returnsの更新
+            returns = self._update_returns(exp_history, returns)
+
+            # 行動価値関数の更新
+            for (s, a) in returns.keys():
+                profits = returns[(s, a)]
+
+                if len(profits) == 0:
+                    self.q_func[(s, a)] = 0
                 else:
-                    profit_sum += profit
+                    self.q_func[(s, a)] = sum(profits) / len(profits)
 
-            if profit_sum is None:
-                continue
+        # 学習後の行動価値関数を表示する
+        pprint(self.q_func)
 
-            avg_profit = profit_sum / self.config['num_playout']
-
-            # 平均収益をその状態sの状態価値関数とする
-            self.v_func[s] = avg_profit
-
-        pprint(self.v_func)
-
-    # プレイアウトの実行
-    def _playout(self, init_state):
-        # 初期状態を設定
-        self.env.set_state(init_state)
-
-        # 初期状態が終端状態であれば、プレイアウトは実施しない
-        if self.env.is_terminal_state():
-            self.env.reset()
-            return None
-
+    # プレイアウトの実施
+    def _playout(self):
         step_count = 0
-        profit = 0 # プレイアウトの収益
-        contribution_rate = 1.0 # 寄与率。収益の計算に使う
+        exp_history = []
         while not self.env.is_terminal_state():
-            # 行動aを選択する
-            action = self._select_action_randomly()
+            # 現在の状態sを取得
+            state = self.env.get_state()
+
+            # ε-greedy法により、行動aを選択する
+            action = self._select_action_with_epsilon_greedy()
 
             # 行動aを行い、報酬rを得る
             reward = self.env.exec_action(action)
-            profit += (reward * contribution_rate)
             step_count += 1
-            contribution_rate *= self.config['gamma']
 
-            # ただし、終端状態での状態価値関数の値はその終端状態での報酬値とする
-            if self.env.is_terminal_state():
-                terminal_state = self.env.get_state()
-                self.v_func[terminal_state] = reward
+            # 経験(s, a, r)を経験履歴に追加
+            # 終端状態では行動しないため、終端状態の経験は追加不要
+            exp = (state, action, reward)
+            exp_history.append(exp)
 
             # 無限ループ防止のため、一定の回数行動しても終端にたどり着かなければ打ち止め
             if step_count > self.config['playout_step_limit']:
@@ -107,39 +99,68 @@ class MonteCarloAgent(AgentBase):
         # 環境をリセット
         self.env.reset()
 
-        return profit
+        return exp_history
 
-    # ランダムに行動を選択する
-    def _select_action_randomly(self):
-        actions = self.env.get_actions()
-        idx = random.randint(0, len(actions) - 1)
+    # ε-greedy法で行動を選択する
+    def _select_action_with_epsilon_greedy(self):
+        v = random.uniform(0, 1)
 
-        return actions[idx]
-
-    # greedy法で行動を選択する
-    def _select_action_with_greedy(self):
-        original_state = self.env.get_state()
-        max_v = None
-        selected_action = None
-
-        # 行動後の状態価値関数の値が最大となるような行動を選択する
-        for action in self.env.get_actions():
-            self.env.exec_action(action)
-            state = self.env.get_state()
-
-            if (max_v is None) or (self.v_func[state] > max_v):
-                selected_action = action
-                max_v = self.v_func[state]
-
-            # 状態を元に戻す
-            self.env.set_state(original_state)
+        if v <= self.config['epsilon']:
+            # ランダム選択
+            actions = self.env.get_action_space()
+            idx = random.randint(0, len(actions) - 1)
+            selected_action = actions[idx]
+        else:
+            # greedy法による選択
+            selected_action = self._select_action_with_greedy()
 
         return selected_action
 
-    # 状態価値関数を初期化して返す
-    def _make_initial_v_function(self):
-        init_v_func = {}
-        for state in self.env.get_state_space():
-            init_v_func[state] = 0
+    # greedy法で行動を選択する
+    def _select_action_with_greedy(self):
+        # Q(s, a)が最大となるようなaは複数存在しうるので、そのような場合は
+        # ランダムにaを選択することにする
+        state = self.env.get_state()
+        actions = self.env.get_action_space()
+        q_values = [self.q_func[(state, a)] for a in actions]
+        greedy_indices = [i for i, q in enumerate(q_values) if q == max(q_values)]
+        greedy_idx = greedy_indices[random.randint(0, len(greedy_indices) - 1)]
+        return actions[greedy_idx]
 
-        return init_v_func
+    # Returnsを更新する
+    def _update_returns(self, exp_history, returns):
+        occurred_states_and_actions = []
+        for i, exp in enumerate(exp_history):
+            state, action, reward = exp
+            # すでに出現した状態+行動であれば、収益を計算しない
+            if (state, action) in occurred_states_and_actions:
+                continue
+
+            # 初めて出現する状態+行動のみ、収益を計算してReturns[(s, a)]に追加
+            profit = self._calc_profit(exp_history[i:])
+            returns[(state, action)].append(profit)
+            occurred_states_and_actions.append((state, action))
+
+        return returns
+
+    # 与えられた経験履歴の収益を計算する
+    # exp_historyは、エピソード全体の経験履歴でなくても良い(部分集合でもよい)
+    def _calc_profit(self, exp_history):
+        # 収益の計算においては、報酬にしか興味がない
+        profit = 0
+        contribution_rate = 1 # 寄与率。ステップt(t=0,1,...,T)において、γ^t
+        for _, _, reward in exp_history:
+            profit += reward * contribution_rate
+            contribution_rate *= self.config['gamma']
+
+        return profit
+
+    # 行動価値関数を初期化して返す
+    def _make_initial_q_function(self):
+        init_q_func = {}
+        s_space = self.env.get_state_space()
+        a_space = self.env.get_action_space()
+        for state, action in product(s_space, a_space):
+            init_q_func[(state, action)] = 0
+
+        return init_q_func
