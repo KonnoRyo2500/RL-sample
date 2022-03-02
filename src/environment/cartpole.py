e348adf45ee726020612f3f630ecd59ad524f614
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-03-03 00:05:45 +0900

    Cartpole環境クラスを作成 中身は空

---
+++ src/environment/cartpole.py
@@ -0,0 +1,34 @@
+# 強化学習勉強用サンプルプログラム Cartpole環境クラス
+
+from environment.env_base import EnvironmentBase
+
+# Cartpole環境クラス
+class Cartpole(EnvironmentBase):
+    # コンストラクタ
+    def __init__(self, config):
+        super().__init__(config)
+
+    # 環境の行動空間を取得
+    def get_action_space(self):
+        pass
+
+    # 指定された行動を実行し、報酬を得る
+    def exec_action(self, action):
+        pass
+
+    # 環境の状態空間を取得
+    # 状態が多すぎて(もしくは無限に存在して)取得できない場合はNoneを返す
+    def get_state_space(self):
+        pass
+
+    # 現在の状態を取得
+    def get_state(self):
+        pass
+
+    # 現在の状態が終端状態かどうかを返す
+    def is_terminal_state(self):
+        pass
+
+    # 環境をリセットする
+    def reset(self):
+        pass
