6f2c3afa524a07ab2072ba6a42ee0c1c39bb9022
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-03-10 04:29:27 +0900

    OpenAI GymのCartpole環境を一通り実装

--- src/environment/cartpole.py
+++ src/environment/cartpole.py
@@ -1,6 +1,32 @@
 # 強化学習勉強用サンプルプログラム Cartpole環境クラス
+# 環境として、OpenAI GymのCartPole-v1を用いる
+
+from enum import Enum
+from itertools import product
+import math
+
+import gym
 
 from environment.env_base import EnvironmentBase
+
+# カートを押す方向
+class PushDirection(Enum):
+    Left = 0 # 左
+    Right = 1 # 右
+
+# 各状態の最小値、最大値
+# カートの位置
+CART_POSITION_MIN = -2.4
+CART_POSITION_MAX = 2.4
+# カートの速度
+CART_VELOCITY_MIN = float('-inf')
+CART_VELOCITY_MAX = float('inf')
+# 棒の角度(度)
+POLE_ANGLE_MIN = -24.8
+POLE_ANGLE_MAX = 24.8
+# 棒の速度
+POLE_VELOCITY_MIN = float('-inf')
+CART_VELOCITY_MAX = float('inf')
 
 # Cartpole環境クラス
 class Cartpole(EnvironmentBase):
@@ -8,31 +34,121 @@ class Cartpole(EnvironmentBase):
     def __init__(self, config):
         super().__init__(config)
 
+        self.env = gym.make('CartPole-v1')
+        self.state = None
+        self.is_terminated = False
+
+        self.state = self._quantize_state(self.env.reset())
+
     # 環境全体における行動空間を取得
     def get_whole_action_space(self):
-        pass
+        return [dir for dir in PushDirection]
 
     # 現状態における行動空間を取得
     def get_current_action_space(self):
-        pass
+        # この環境では、行動空間は状態に依らない
+        return self.get_whole_action_space()
 
     # 指定された行動を実行し、報酬を得る
     def exec_action(self, action):
-        pass
+        # 行動の実行
+        next_state, reward, is_terminated, dbg_info = self.env.step(action.value)
+
+        # 次状態の決定
+        rad2degree = lambda rad_angle: rad_angle * (180.0 / math.pi)
+        next_state = list(next_state)
+        next_state[2] = rad2degree(next_state[2])
+        next_state = self._quantize_state(next_state)
+
+        # 次状態の適用
+        self.state = next_state
+        self.is_terminated = is_terminated
+
+        return reward
 
     # 環境の状態空間を取得
     # 状態が多すぎて(もしくは無限に存在して)取得できない場合はNoneを返す
+    # Note: 各状態の分割数を増やしすぎると、状態空間のサイズが大きくなるため注意!
     def get_state_space(self):
-        pass
+        # 状態の分割数の範囲を取得
+        rng_pos = range(self.config['position_part_num'])
+        rng_cart_v = range(self.config['cart_velocity_part_num'])
+        rng_angle = range(self.config['pole_angle_part_num'])
+        rng_pole_v = range(self.config['pole_velocity_part_num'])
+
+        # 状態空間の作成
+        state_space = [(p, vc, a, vp) for p, vc, a, vp in product(rng_pos, rng_cart_v, rng_angle, rng_pole_v)]
+
+        return state_space
 
     # 現在の状態を取得
     def get_state(self):
-        pass
+        return self.state
 
     # 現在の状態が終端状態かどうかを返す
     def is_terminal_state(self):
-        pass
+        return self.is_terminated
 
     # 環境をリセットする
     def reset(self):
-        pass
+        self.env.reset()
+        self.is_terminated = False
+
+    # 状態を量子化する
+    # 状態が連続値なので、シンプルなアルゴリズムでもこの環境を利用できるようにする
+    def _quantize_state(self, original_state):
+        # 詳細な状態の取得
+        cart_position, cart_velocity, pole_angle, pole_velocity = original_state
+
+        # カート位置の量子化
+        qs_pos = self._quantize_range(
+            self.config['cart_position_min'],
+            self.config['cart_position_max'],
+            self.config['position_part_num'],
+            cart_position)
+
+        # カート速度の量子化
+        qs_cart_v = self._quantize_range(
+            self.config['cart_velocity_min'],
+            self.config['cart_velocity_max'],
+            self.config['cart_velocity_part_num'],
+            cart_velocity)
+
+        # 棒の角度の量子化
+        qs_angle = self._quantize_range(
+            self.config['pole_angle_min'],
+            self.config['pole_angle_max'],
+            self.config['pole_angle_part_num'],
+            pole_angle)
+
+        # 棒の速度の量子化
+        qs_pole_v = self._quantize_range(
+            self.config['pole_velocity_min'],
+            self.config['pole_velocity_max'],
+            self.config['pole_velocity_part_num'],
+            pole_velocity)
+
+        # 量子化された状態を得る
+        quantized_state = (qs_pos, qs_cart_v, qs_angle, qs_pole_v)
+
+        return quantized_state
+
+    # 値の範囲を指定された個数に等分割し、連続値を量子化する
+    # 注) min, maxは組み込み関数名で存在するため、あえて引数名は"minimum", "maximum"としている
+    def _quantize_range(self, minimum, maximum, num_part, val):
+        # [minimum, maximum)の範囲内でなければ、はみ出た側の区間に
+        # 属しているものとみなす
+        if val < minimum:
+            return 0
+        elif maximum <= val:
+            return num_part - 1
+
+        # 範囲の長さと区間の長さを取得する
+        range_len = maximum - minimum
+        section_len = range_len / num_part
+
+        # この式で計算することで、forループ+if文を使わずとも
+        # 区間のインデックスを求めることができる
+        section_idx = int((val - minimum) // section_len)
+
+        return section_idx
--- src/main.py
+++ src/main.py
@@ -4,12 +4,15 @@ import argparse
 
 from common.config import read_config
 from environment.grid_world import GridWorld
+from environment.cartpole import Cartpole
 from agent.q_agent import QAgent
 from agent.sarsa_agent import SarsaAgent
 from agent.monte_carlo_agent import MonteCarloAgent
 
 # アルゴリズム名
 METHODS = ['q_learning', 'sarsa', 'monte_carlo']
+# 環境名
+ENVS = ['grid_world', 'cartpole']
 
 # コマンドライン引数の解析
 def parse_args():
@@ -20,6 +23,12 @@ def parse_args():
         default='..\\config\\config.yaml',
         help='設定ファイル(YAML)のパス。')
     parser.add_argument(
+        '--env',
+        type=str,
+        default='grid_world',
+        choices=ENVS,
+        help='利用する環境。')
+    parser.add_argument(
         '--method',
         type=str,
         default='q_learning',
@@ -27,6 +36,18 @@ def parse_args():
         help='利用する強化学習アルゴリズム。')
     args = parser.parse_args()
     return args
+
+# 環境の作成
+def create_env(name, config):
+    env_classes = [GridWorld, Cartpole]
+    env_configs = [config['grid_world'], config['cartpole']]
+
+    env_class = dict(zip(ENVS, env_classes))[name]
+    env_config = dict(zip(ENVS, env_configs))[name]
+
+    env = env_class(env_config)
+
+    return env
 
 # エージェントの作成
 def create_agent(method, env, config):
@@ -47,10 +68,10 @@ def main():
     config = read_config(args.config)
 
     # 環境の作成
-    gw = GridWorld(config=config['environment']['grid_world'])
+    env = create_env(args.env, config['environment'])
 
     # エージェントの作成
-    agent = create_agent(args.method, gw, config['agent'])
+    agent = create_agent(args.method, env, config['agent'])
 
     # 選択したエージェントで学習+プレイ
     agent.train()
