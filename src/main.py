eb7075189a147840d9801390edcf5c7fd91eef6a
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-08-04 00:14:51 +0900

    設定ファイルを分割 各種環境・エージェントごとに設定ファイルを持つようにした それに伴い、--configオプションを廃止

--- src/main.py
+++ src/main.py
@@ -2,6 +2,7 @@
 
 import argparse
 from enum import Enum
+import os.path as op
 
 from common.config import read_config
 import environment
@@ -33,14 +34,12 @@ ENV2CLS = {
     Environment.Cartpole.value: environment.Cartpole,
 }
 
+# 設定ファイル名
+CONFIG_NAME = 'config.yaml'
+
 # コマンドライン引数の解析
 def parse_args():
     parser = argparse.ArgumentParser()
-    parser.add_argument(
-        '--config',
-        type=str,
-        default='..\\config\\config.yaml',
-        help='設定ファイル(YAML)のパス。')
     parser.add_argument(
         '--env',
         type=str,
@@ -58,15 +57,22 @@ def parse_args():
 
 # メイン処理
 def main():
-    # 設定値の取得
+    # コマンドライン引数の取得
     args = parse_args()
-    config = read_config(args.config)
+
+    # 設定ファイルの読み込み
+    config_root = op.join(op.dirname(__file__), '..', 'config') # リポジトリルート直下、"config"フォルダ
+    env_config_path = op.join(config_root, 'environment', args.env, CONFIG_NAME)
+    agent_config_path = op.join(config_root, 'agent', args.method, CONFIG_NAME)
+
+    env_config = read_config(env_config_path)
+    agent_config = read_config(agent_config_path)
 
     # 環境の作成
-    env_instance = ENV2CLS[args.env](config['environment'][args.env])
+    env_instance = ENV2CLS[args.env](env_config)
 
     # エージェントの作成
-    agent_instance = ALGO2CLS[args.method](env_instance, config['agent'][args.method])
+    agent_instance = ALGO2CLS[args.method](env_instance, agent_config)
 
     # 選択したエージェントで学習+プレイ
     agent_instance.train()
