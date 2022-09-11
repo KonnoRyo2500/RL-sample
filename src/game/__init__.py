2ef2ef5d09575d772ed5ded7feba6b1c912c57a3
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-09-11 19:52:46 +0900

    各種ゲームフレームワーククラスをフォルダ分け

--- src/game/__init__.py
+++ src/game/__init__.py
@@ -1,2 +1,2 @@
-from game.single_player_game import SinglePlayerGame
-from game.one_vs_one_game import OneVsOneGame
+from game.single_player.single_player_game import SinglePlayerGame
+from game.one_vs_one.one_vs_one_game import OneVsOneGame
