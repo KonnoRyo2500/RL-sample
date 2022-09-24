f7ad71cdcee345fa3efe0d75d68815b784fcbbac
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-09-24 15:34:12 +0900

    複数人プレイヤーゲームフレームワークを一通り実装 オセロ環境が実装できていないため、動作確認はできていない

--- src/game/implementation/multi_player/multi_player_game.py
+++ src/game/implementation/multi_player/multi_player_game.py
@@ -10,10 +10,58 @@ class MultiPlayerGame(GameBase):
     def __init__(self, env, agents):
         super().__init__(env, agents, Game.MultiPlayer.value)
 
+        self.player = 0  # 現手番でのプレイヤー
+        self.env.change_player(self.player)
+
     # エージェントを学習させる
     def train_agent(self):
-        pass
+        # すべてのプレイヤーが終端状態になるまで、ゲームを続ける
+        is_terminated = [False] * len(self.agents)
+        while not all(is_terminated):
+            # 現在のプレイヤーがすでに終端状態にある
+            if self.env.is_terminated():
+                is_terminated[self.player] = True
+                self._switch_to_next_player()
+                continue
+
+            # エージェントが次の行動を決定する
+            agent = self.agents[self.player]
+            action = agent.decide_action(self.env)
+
+            # 決定した行動を環境に対して行い、報酬を得る
+            reward = self.env.exec_action(action)
+
+            # エージェントは、得られた報酬と変化した環境の状態をもとに自身のパラメータを更新する
+            agent.feedback(reward, self.env)
+
+            # 次のプレイヤーに交代
+            self._switch_to_next_player()
 
     # 学習済みのエージェントでゲームをプレイする
     def play(self):
-        pass
+        # すべてのプレイヤーが終端状態になるまで、ゲームを続ける
+        is_terminated = [False] * len(self.agents)
+        while not all(is_terminated):
+            # 現在のプレイヤーがすでに終端状態にある
+            if self.env.is_terminated():
+                is_terminated[self.player] = True
+                self._switch_to_next_player()
+                continue
+
+            # エージェントが次の行動を決定する
+            agent = self.agents[self.player]
+            action = agent.decide_action(self.env)
+
+            # 決定した行動を環境に対して行い、報酬を得る
+            reward = self.env.exec_action(action)
+
+            # 行動を表示する
+            print(f'行動{action}を選択し、報酬{reward}が得られました')
+
+            # 次のプレイヤーに交代
+            self._switch_to_next_player()
+
+    # 次のプレイヤーに交代する
+    def _switch_to_next_player(self):
+        self.player = (self.player + 1) % len(self.agents)
+        self.env.change_player(self.player)
