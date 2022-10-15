ca3887bfa22d26a2a34c60e60ebef7c432588f54
Konno Ryo <russian573-andr906@ezweb.ne.jp>
2022-10-15 23:01:29 +0900

    学習の進行状況をtqdmを用いて視覚的に表示できるようにした 各エピソードの合計報酬の平均も表示するようにした

--- src/game/implementation/multi_player/multi_player_game.py
+++ src/game/implementation/multi_player/multi_player_game.py
@@ -1,5 +1,7 @@
 # 強化学習勉強用サンプルプログラム 複数人プレイヤーゲームフレームワーククラス
 
+from tqdm import tqdm
+
 from game.base.game_base import GameBase
 from common.const_val import Game
 
@@ -12,14 +14,25 @@ class MultiPlayerGame(GameBase):
 
     # エージェントを学習させる
     def train_agent(self):
-        for i in range(self.config.num_episode):
-            self._train_episode()
-            self.env.reset()
+        total_reward = 0
+
+        with tqdm(range(self.config.num_episode), desc='[学習中]') as train_progress_bar:
+            for i in train_progress_bar:
+                # エピソードを実施
+                total_reward += self._train_episode()
+                self.env.reset()
+
+                # 各エピソードの合計報酬の平均を更新し、表示する
+                # tqdmは表示する値の桁数が増えると小数点以下の値を表示しなくなるため、文字列にして渡す
+                avg_reward = total_reward / (i + 1)
+                train_progress_bar.set_postfix({'平均報酬': f"{avg_reward:.2f}"})
 
     # 学習済みのエージェントでゲームをプレイする
     def play(self):
         # すべてのプレイヤーが終端状態になるまで、ゲームを続ける
         is_terminated = [False] * len(self.agents)
+
+        print('プレイ開始')
         while not all(is_terminated):
             # 現在のプレイヤーがすでに終端状態にある
             player = self.env.get_player()
@@ -46,8 +59,10 @@ class MultiPlayerGame(GameBase):
 
     # 学習時の1エピソード
     def _train_episode(self):
-        # すべてのプレイヤーが終端状態になるまで、ゲームを続ける
         is_terminated = [False] * len(self.agents)
+        total_reward = 0
+
+        # すべてのプレイヤーが終端状態になるまで、ゲームを続ける
         while not all(is_terminated):
             # 現在のプレイヤーがすでに終端状態にある
             player = self.env.get_player()
@@ -62,9 +77,12 @@ class MultiPlayerGame(GameBase):
 
             # 決定した行動を環境に対して行い、報酬を得る
             reward = self.env.exec_action(action)
+            total_reward += reward
 
             # エージェントは、得られた報酬と変化した環境の状態をもとに自身のパラメータを更新する
             agent.feedback(reward, self.env)
 
             # 次のプレイヤーに交代
             self.env.switch_to_next_player()
+
+        return total_reward
--- src/game/implementation/single_player/single_player_game.py
+++ src/game/implementation/single_player/single_player_game.py
@@ -1,5 +1,7 @@
 # 強化学習勉強用サンプルプログラム 一人用ゲームフレームワーククラス
 
+from tqdm import tqdm
+
 from game.base.game_base import GameBase
 from common.const_val import Game
 
@@ -23,35 +25,54 @@ class SinglePlayerGame(GameBase):
     # エージェントを学習させる
     def train_agent(self):
         self.agent.switch_to_train_mode()
+        total_reward = 0
 
-        for i in range(self.config.num_episode):
-            self._train_episode()
-            self.env.reset()
+        with tqdm(range(self.config.num_episode), desc='[学習中]') as train_progress_bar:
+            for i in train_progress_bar:
+                # エピソードを実施
+                total_reward += self._train_episode()
+                self.env.reset()
+
+                # 各エピソードの合計報酬の平均を更新し、表示する
+                # tqdmは表示する値の桁数が増えると小数点以下の値を表示しなくなるため、文字列にして渡す
+                avg_reward = total_reward / (i + 1)
+                train_progress_bar.set_postfix({'平均報酬': f"{avg_reward:.2f}"})
 
     # 学習済みのエージェントでゲームをプレイする
     def play(self):
         self.agent.switch_to_play_mode()
+        total_reward = 0
 
+        print('プレイ開始')
         while not self.env.is_terminal_state():
             # エージェントが行動を決定する
             action = self.agent.decide_action(self.env)
 
             # 決定した行動を環境に対して行い、報酬を得る
             reward = self.env.exec_action(action)
+            total_reward += reward
 
             # 行動を表示する
             print(f'行動{action}を選択し、報酬{reward}が得られました')
+
+        # 合計報酬の表示
+        print(f'合計報酬: {total_reward}')
 
         self.env.reset()
 
     # 1エピソード分の学習を行う
     def _train_episode(self):
+        total_reward = 0
+
         while not self.env.is_terminal_state():
             # エージェントが次の行動を決定する
             action = self.agent.decide_action(self.env)
 
             # 決定した行動を環境に対して行い、報酬を得る
             reward = self.env.exec_action(action)
+            total_reward += reward
 
             # エージェントは、得られた報酬と変化した環境の状態をもとに自身のパラメータを更新する
             self.agent.feedback(reward, self.env)
+
+        return total_reward
